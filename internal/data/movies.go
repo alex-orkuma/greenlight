33952506032519b454d9963f16c17ff15f51aebc
alex-orkuma <orkumashi@gmail.com>
2021-11-15 21:35:08 +0100

    added a movie and enclasulated it into a parent models

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/alex-orkuma/greenlight/internal/validator"
@@ -29,4 +30,24 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 	v.Check(len(movie.Genres) <= 5, "genre", "must be not contain more than 5 genres")
 	v.Check(v.Unique(movie.Genres), "genre", "must not contain duplicate values")
 
+}
+
+type MovieModel struct {
+	DB *sql.DB
+}
+
+func (m MovieModel) Insert(movie *Movie) error {
+	return nil
+}
+
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	return nil, nil
+}
+
+func (m MovieModel) Update(movie *Movie) error {
+	return nil
+}
+
+func (m MovieModel) Delete(id int64) error {
+	return nil
 }
